59cfafbb46b03222b6c93ba6840b2fa3ea48d461
W4we1 <Wawel2004@gmail.com>
2022-10-12 15:00:24 +0200

    3 commit

--- zbs.sql
+++ zbs.sql
@@ -3,4 +3,8 @@ SELECT * FROM students
 
 2.ii
 SELECT * FROM students
-JOIN test ON students.id =
+JOIN test ON students.id = test.students_id
+JOIN students_has_test ON students.id = students_has_test.students_id
+WHERE students.id = 1
+
+3.iii
